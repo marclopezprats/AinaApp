3125fcd1659b9799ab1e70ce6c971fe73c493f57
marclopezprats <125487348+marclopezprats@users.noreply.github.com>
2024-06-25 20:24:55 +0200

    experience

--- frontend/src/pages/AinaHome/index.js
+++ frontend/src/pages/AinaHome/index.js
@@ -19,9 +19,7 @@ import { useTranslation } from 'react-i18next';
 import 'flag-icons/css/flag-icons.min.css';
 import Whatsapp from './whatsapp';
 import Divider from '@mui/material/Divider';
-import KarveIframe  from './ReservationWidget';
-
-
+import KarveIframe from './ReservationWidget';
 
 const DefaultFooter = lazy(() => import("examples/Footers/DefaultFooter"));
 const InstagramPosts = lazy(() => import('./InstagramPosts'));
@@ -32,10 +30,9 @@ const MobileDrawer = lazy(() => import('./MobileDrawer'));
 const MaxWidthDialog = lazy(() => import('./PopUp'));
 const WhyAinaCar_ = lazy(() => import('./PorQue'));
 
-function AinaHome() {   
+function AinaHome() {
   const masonryRef = useRef(null);
   const accessToken = process.env.REACT_APP_INSTA_ACCESS_TOKEN;
-  console.log('token',accessToken)
   const buttonRef = useRef(null);
   const [formData, setFormData] = useState({
     nombre: '',
@@ -66,9 +63,8 @@ function AinaHome() {
   const [valorHijo, setValorHijo] = useState('');
   const manejarCambio = (nuevoValor) => {
     setValorHijo(nuevoValor);
-    console.log(nuevoValor);
   };
-  
+
   const [appBarHeight, setAppBarHeight] = useState(0);
   const appBarRef = useRef(null);
 
@@ -91,12 +87,12 @@ function AinaHome() {
 
   return (
     <>
-      <Whatsapp/>
+      <Whatsapp />
       <MKBox bgColor="#d6061e" sx={{ minHeight: "5vh", textAlign: "center", justifyContent: "center", alignItems: "center", display: "flex" }} zIndex={2}>
         <MKTypography color="white" sx={{}} variant="h6">{t('recogida247')}</MKTypography>
       </MKBox>
       <Box ref={appBarRef} style={{ position: 'sticky', top: valorHijo ? '15px' : '60px', transition: valorHijo ? 'top 1s ease-in' : 'top 0.0s ease-out', zIndex: 3 }}>
-          <ResponsiveAppBar toggleDrawer={toggleDrawer} onCambio={manejarCambio} />
+        <ResponsiveAppBar toggleDrawer={toggleDrawer} onCambio={manejarCambio} />
       </Box>
       <Box
         sx={{
@@ -110,34 +106,36 @@ function AinaHome() {
           alignItems: "center",
           marginTop: `-${appBarHeight}px`
         }}>
-        <MKBox sx={{ justifyContent: "bottom", display: "flex", flexDirection: "column", position: "relative" }}>
+        <MKBox sx={{ justifyContent: "bottom", display: "flex", flexDirection: "column", position: "relative", padding: { xs: "20px 0", sm: "40px 0" } }}>
           <Container>
-            <Grid sx={{ marginTop: { xs: `${appBarHeight*2}px`, sm: `${appBarHeight*2}px`, md: `${appBarHeight*2}px`, lg: `${appBarHeight}px` }}} container justifyContent="center">
-              <Grid item xs={12} sm={12} md={12} xl={7.5} sx={{ display: 'flex', alignItems: 'center' }}>
+            <Grid sx={{ marginTop: { xs: `${appBarHeight * 2}px`, sm: `${appBarHeight * 2}px`, md: `${appBarHeight * 2}px`, lg: `${appBarHeight}px` } }} container justifyContent="center">
+              <Grid item xs={12} sm={12} md={12} xl={7.5} sx={{ display: 'flex', alignItems: 'center', padding: { xs: '0 10px' } }}>
                 <MKBox sx={{ maxWidth: '100%' }}>
-                  <MKTypography 
-                    color="white" 
-                    variant="h1" 
-                    sx={{ 
-                      fontFamily: 'Rodina-Regular', 
-                      textAlign: 'left', 
+                  <MKTypography
+                    color="white"
+                    variant="h1"
+                    sx={{
+                      fontFamily: 'Rodina-Regular',
+                      textAlign: 'left',
                       lineHeight: 1,
                       fontSize: {
                         xs: '2.5rem',
                         sm: '3rem',
                         md: '4rem',
                         lg: '5rem'
-                      }
+                      },
+                      marginTop: { xs: '20px', sm: '30px' } // Mueve el título hacia abajo en móvil
                     }}
                   >
                     {t('welcome')}
                   </MKTypography>
-                  <MKTypography 
-                    color="white" 
-                    variant="h3" 
-                    sx={{ 
-                      fontFamily: 'Rodina-Regular', 
-                      textAlign: 'left', 
+                  <MKTypography
+                    color="white"
+                    variant="h3"
+                    sx={{
+                      marginBottom: { xs: '30px', sm: '30px' },
+                      fontFamily: 'Rodina-Regular',
+                      textAlign: 'left',
                       lineHeight: 1.5,
                       fontSize: {
                         xs: '1.5rem',
@@ -154,12 +152,7 @@ function AinaHome() {
 
               <Grid item xs={12} sm={12} md={12} xl={4.5} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center', height: '100%' }}>
                 <Suspense fallback={<div>Loading...</div>}>
-                
-                <KarveIframe />
-                
-
-
-
+                  <KarveIframe sx={{ marginTop: { xs: '30px', sm: '30px' }, marginBottom: { xs: '20px', sm: '30px' } }} />
                 </Suspense>
               </Grid>
             </Grid>
@@ -176,12 +169,12 @@ function AinaHome() {
         justifyContent="center"
         style={{ minHeight: "5vh", cursor: 'pointer' }}
         //onClick={handleScrollToWhyAinaCar}
-        >
-        <MKTypography style={{ }} color="white" variant="h6">
+      >
+        <MKTypography style={{}} color="white" variant="h6">
           {t('mas_info')}
         </MKTypography>
         <IconButton variant="contained">
-          <ExpandMoreIcon style={{ color:'white' }} /> 
+          <ExpandMoreIcon style={{ color: 'white' }} />
         </IconButton>
       </MKBox>
       <Suspense fallback={<div>Loading...</div>}>
@@ -252,7 +245,7 @@ function AinaHome() {
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
-          }}/>
+          }} />
           <Container>
             <Suspense fallback={<div>Loading...</div>}>
               <InstagramPosts accessToken={accessToken} />
