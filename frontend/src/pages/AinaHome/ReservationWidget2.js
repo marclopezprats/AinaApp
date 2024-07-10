c757b294532e085b6468842b44bf069150387554
marclopezprats <125487348+marclopezprats@users.noreply.github.com>
2024-07-10 14:16:55 +0200

    test

--- frontend/src/pages/AinaHome/ReservationWidget2.js
+++ frontend/src/pages/AinaHome/ReservationWidget2.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import React, { useEffect } from 'react';
 
 const ReservationWidgetTest = () => {
   useEffect(() => {
